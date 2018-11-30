5050abcbef25735e95376b35514855d8043f7cbf
NaydanovPY <p.naydanovgoo@gmail.com>
2018-11-30 15:10:31 +0700

    add code camera

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createAppContainer } from 'react-navigation';
 import { Font, AppLoading } from 'expo';
+import Camera from 'react-native-camera';
+import { Text, StyleSheet } from 'react-native';
 
 import Router from 'screens/router';
 import Api from 'shared/services/api';
@@ -9,6 +11,26 @@ import configureStore from './configureStore';
 
 const api = new Api();
 const store = configureStore({ api });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  view: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItens: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'streetblue',
+    borderRadius: 10,
+    color: 'red',
+    padding: 15,
+    margin: 45,
+  },
+});
 
 class App extends Component {
   constructor() {
@@ -25,17 +47,37 @@ class App extends Component {
 
   render() {
     const { isReady } = this.state;
-    const AppContainer = createAppContainer(Router);
+    // const AppContainer = createAppContainer(Router);
 
     return (
       !isReady
         ? <AppLoading />
         : (
-          <Provider store={store}>
-            <AppContainer />
+          <Provider store={store} style={styles.container}>
+            {/* <AppContainer /> */}
+            <Camera
+              ref={cam => { this.camera = cam; }}
+              style={styles.view}
+              aspect={Camera.constants.Aspect.fill}>
+              <Text
+                style={styles.capture}
+                onPress={this.takePicture.bind(this)}
+              >
+                CAPTURE_IMAFE
+              </Text>
+            </Camera>
           </Provider>
         )
     );
+  }
+
+  takePicture() {
+    const options = {};
+    this.camera.capture({ metadata: options }).then(data => {
+      console.log(data);
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
   /** Загружает шрифты для native-base */
